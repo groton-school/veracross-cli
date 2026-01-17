baf7eca491ce9492ab4f9cba5f88018d31f717cb
Seth Battis <seth@battis.net>
2026-01-17 12:32:52 -0500

    refactor: fix regex

--- packages/oauth2-cli/veracross/src/Client.ts
+++ packages/oauth2-cli/veracross/src/Client.ts
@@ -1,5 +1,5 @@
-import * as OAuth2 from "@oauth2-cli/qui-cli/dist/OAuth2.js";
-import path from "node:path";
+import * as OAuth2 from '@oauth2-cli/qui-cli/dist/OAuth2.js';
+import path from 'node:path';
 
 export type Credentials = OAuth2.Credentials & {
   school_route: string;
@@ -13,16 +13,16 @@ export class Client extends OAuth2.Client {
     this.school_route = school_route;
   }
 
-  public request(...args: Parameters<OAuth2.Client["request"]>) {
+  public request(...args: Parameters<OAuth2.Client['request']>) {
     let [url] = args;
     const [, ...rest] = args;
-    if (typeof url === "string" && !url.startsWith("https://")) {
+    if (typeof url === 'string' && !url.startsWith('https://')) {
       url = new URL(
         path.join(
           this.school_route,
-          url.replace(new RegExp(`^/(?${this.school_route}/)?`), ""),
+          url.replace(new RegExp(`^/?(${this.school_route}/)?`), '')
         ),
-        "https://api.veracross.com",
+        'https://api.veracross.com'
       );
     }
     return super.request(url, ...rest);

5958e6ef7fa8d375b27763d57a8a355eae1b059c
Seth Battis <seth@battis.net>
2026-01-17 13:19:52 -0500

    refactor: parse the response

--- examples/dev-command/src/index.ts
+++ examples/dev-command/src/index.ts
@@ -2,6 +2,6 @@ import { Veracross } from '@oauth2-cli/veracross';
 import { Core } from '@qui-cli/core';
 import { Log } from '@qui-cli/log';
 
+Veracross.configure({ scope: 'contact_info:read' });
 await Core.run();
-Veracross.configure({ scope: 'person_accounts:read' });
-Log.info(await Veracross.request('v3/person_accounts/2'));
+Log.info(await Veracross.requestJSON('v3/contact_info/2'));

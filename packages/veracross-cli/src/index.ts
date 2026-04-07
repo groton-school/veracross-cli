a0d4cf4ea8ced5a493e679518ce97dead248dbd7
Seth Battis <seth@battis.net>
2026-04-07 08:30:10 -0400

    fix: include reason for Veracross API authorization

--- packages/veracross-cli/src/index.ts
+++ packages/veracross-cli/src/index.ts
@@ -1,5 +1,8 @@
+import { Veracross } from '@oauth2-cli/veracross';
 import { build } from '@qui-cli/structured';
 import path from 'node:path';
+
+Veracross.configure({ reason: 'vc' });
 
 await build({
   fileName: import.meta.filename,

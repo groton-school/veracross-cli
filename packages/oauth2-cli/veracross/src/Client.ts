fd47b3ce557517fbfb89e4a6b33a36d14cdd05ce
Seth Battis <seth@battis.net>
2026-04-20 18:36:08 -0400

    refactor: clearer syntax and naming

--- packages/oauth2-cli/veracross/src/Client.ts
+++ packages/oauth2-cli/veracross/src/Client.ts
@@ -1,6 +1,9 @@
 import * as OAuth2 from '@oauth2-cli/qui-cli/extendable/index.js';
 import { Log } from '@qui-cli/log';
-import createClient, { Middleware } from 'openapi-fetch';
+import createClient, {
+  Middleware,
+  MiddlewareCallbackParams
+} from 'openapi-fetch';
 import { paths } from './Data-API.js';
 
 export type Credentials = OAuth2.Credentials & {
@@ -15,19 +18,20 @@ export class Client<C extends Credentials> extends OAuth2.Client<C> {
     this.Data = createClient<paths>({
       baseUrl: `https://api.veracross.com/${this.credentials.school_route}/v3`
     });
-    this.Data.use(this.middleware());
+    this.Data.use(this.authorizationMiddleware());
   }
 
-  protected middleware(): Middleware {
-    const getToken = this.getToken;
+  private async onRequest({ request }: MiddlewareCallbackParams) {
+    request.headers.set(
+      'Authorization',
+      `Bearer ${(await this.getToken()).access_token}`
+    );
+    return request;
+  }
+
+  private authorizationMiddleware(): Middleware {
     return {
-      async onRequest({ request }) {
-        request.headers.set(
-          'Authorization',
-          `Bearer ${(await getToken()).access_token}`
-        );
-        return request;
-      }
+      onRequest: this.onRequest.bind(this)
     };
   }
 }

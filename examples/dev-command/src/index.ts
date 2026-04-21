a5082e274f569531ff537b7c4abc18e6594473ee
Seth Battis <seth@battis.net>
2026-04-20 20:20:11 -0400

    docs: include authorization reason in example

--- examples/dev-command/src/index.ts
+++ examples/dev-command/src/index.ts
@@ -2,7 +2,10 @@ import { Veracross } from '@oauth2-cli/veracross';
 import { Core } from '@qui-cli/core';
 import { Log } from '@qui-cli/log';
 
-Veracross.configure({ credentials: { scope: 'contact_info:read' } });
+Veracross.configure({
+  reason: 'dev-command',
+  credentials: { scope: 'contact_info:read' }
+});
 await Core.run();
 Log.info({
   info: await Veracross.client().Data.GET('/contact_info/{id}', {

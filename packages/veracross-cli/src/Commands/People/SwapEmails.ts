2b5eff1c78cec16f0ea0049b5666eff45f0a9c0a
Seth Battis <seth@battis.net>
2026-07-20 10:47:58 -0400

    feat!: reworked swapEmails to use a CSV list instead of specific roles for flexibility
    BREAKING: `--role` is no longer a valid argument, a positional argument,
    `pathToCsv` is now required.

--- packages/veracross-cli/src/Commands/People/SwapEmails.ts
+++ packages/veracross-cli/src/Commands/People/SwapEmails.ts
@@ -1,25 +1,33 @@
+import { PathString } from '@battis/descriptive-types';
 import { Veracross } from '@oauth2-cli/veracross';
 import { Colors } from '@qui-cli/colors';
+import { Positionals } from '@qui-cli/core';
 import { Log } from '@qui-cli/log';
 import * as Plugin from '@qui-cli/plugin';
+import { parse } from 'csv/sync';
 import ora from 'ora';
+import fs from 'node:fs';
+import path from 'node:path';
 
 export type Configuration = Plugin.Configuration & {
-  roles?: number[];
+  pathToCsv?: PathString;
   dryRun?: boolean;
 };
 
+type ContactInfo = { person_id: number; email_1?: string; email_2?: string };
+
+Positionals.require({
+  pathToCsv: {
+    description: `Path to CSV file containing at least the column ${Colors.varName('person_id')}. If the columns ${Colors.varName('email_1')} and/or ${Colors.varName('email_2')} are present they will be used as the new values for those fields.`
+  }
+});
+Positionals.allowOnlyNamedArgs();
+
 export const name = 'swapEmails';
 
-const scope = [
-  'person_accounts:list',
-  'contact_info:read',
-  'contact_info:update'
-];
+const scope = ['contact_info:read', 'contact_info:update'];
 
-const PAGE_SIZE = 100;
 const config: Configuration = {
-  roles: [7], // Future Student,
   dryRun: false
 };
 
@@ -35,15 +43,18 @@ export function options(): Plugin.Options {
   return {
     man: [
       { level: 1, text: 'Swap Emails Options' },
+      {
+        text:
+          `Swap ${Colors.varName('email_1')} and ${Colors.varName('email_2')} ` +
+          `fields on user contact info, or replace them entirely with values ` +
+          `provided from the CSV file provided. If the CSV file contains ` +
+          `either email column, the existing data inthe database for that ` +
+          `column will be overwritten (and the other column will onl be ` +
+          `updated if that column is provided in the CSV file)`
+      },
       { level: 2, text: 'Required Veracross API scopes' },
       ...scope.map((s) => ({ text: Colors.value(s) }))
     ],
-    numList: {
-      role: {
-        description: `The role ID for the security roles of the users to be affected ${Colors.url('https://axiom.veracross.com/#/results/74')}`,
-        default: config.roles
-      }
-    },
     flag: {
       dryRun: {
         description: 'Run a dry-run of the script without making changes',
@@ -54,10 +65,9 @@ export function options(): Plugin.Options {
 }
 
 export function init({ values }: Plugin.ExpectedArguments<typeof options>) {
-  const { role: roles, ...rest } = values;
-  configure({ roles, ...rest });
+  configure({ pathToCsv: Positionals.get('pathToCsv'), ...values });
   Veracross.configure({
-    reason: 'vc persons swapEmail',
+    reason: 'vc people swapEmail',
     credentials: {
       scope
     }
@@ -65,84 +75,78 @@ export function init({ values }: Plugin.ExpectedArguments<typeof options>) {
 }
 
 export async function run() {
-  for (const role of config.roles || []) {
-    let page = 1;
-    let done = false;
-    do {
-      const {
-        data: { data = [] } = {},
-        error,
-        response
-      } = await Veracross.Data().GET('/person_accounts', {
-        params: {
-          query: { role },
-          header: { 'X-Page-Number': page, 'X-Page-Size': PAGE_SIZE }
-        }
+  if (!config.pathToCsv) {
+    Log.error(`${Colors.positionalArg('pathToCsv')} must be defined`);
+    process.exit(1);
+  }
+  const data = parse<ContactInfo>(
+    fs.readFileSync(path.resolve(process.cwd(), config.pathToCsv), 'utf8'),
+    { columns: true }
+  );
+
+  for (const row of data) {
+    const spinner = ora(`Person ID ${row.person_id}`).start();
+    const { data: { data: contact_info } = {}, error } =
+      await Veracross.Data().GET('/contact_info/{id}', {
+        params: { path: { id: row.person_id } }
       });
-      for (const person of data) {
-        const spinner = ora(person.username).start();
-        const { data: { data: contact_info } = {} } =
-          await Veracross.Data().GET('/contact_info/{id}', {
-            params: {
-              path: { id: person.person_id }
-            }
-          });
-        if (contact_info?.email_1 && contact_info?.email_2) {
-          if (config.dryRun) {
-            if (contact_info.email_1 && contact_info.email_2) {
-              spinner.info(
-                `${person.username} will swap ${contact_info.email_1} and ${contact_info.email_2}`
-              );
-            } else {
-              spinner.warn(
-                `${person.username} is missing ${
-                  contact_info.email_1
-                    ? 'email_2'
-                    : contact_info.email_2
-                      ? 'email_1'
-                      : 'email_1 and email_2'
-                }`
-              );
-            }
-          } else {
-            const { error } = await Veracross.Data().PATCH(
-              '/contact_info/{id}',
-              {
-                params: {
-                  path: { id: person.person_id }
-                },
-                body: {
-                  data: {
-                    email_1: contact_info.email_2,
-                    email_2: contact_info.email_1
-                  }
-                }
-              }
-            );
-            if (error) {
-              spinner.fail(
-                `Error swapping emails for ${person.username}:\n${Log.syntaxColor(error)}`
-              );
-            } else {
-              spinner.succeed(
-                `${Colors.value(contact_info.name)} swapped (${Colors.varName('email_1')}:${Colors.url(contact_info.email_2)}; ${Colors.varName('email_2')}:${Colors.url(contact_info.email_1)})`
-              );
+    if (error) {
+      spinner.fail(
+        `${spinner.text}: ${Colors.error(`Error ${error.error_id}: ${error.error}`)}`
+      );
+      continue;
+    }
+    if (contact_info) {
+      spinner.text = contact_info?.name;
+      let email_1: string | undefined = undefined;
+      let email_2: string | undefined = undefined;
+      if (row.email_1) {
+        email_1 = row.email_1;
+      }
+      if (row.email_2) {
+        email_2 = row.email_2;
+      }
+      if (!email_1 && !email_2) {
+        email_1 = contact_info.email_2;
+        email_2 = contact_info.email_1;
+      }
+      if (!config.dryRun) {
+        const { error } = await Veracross.Data().PATCH('/contact_info/{id}', {
+          params: {
+            path: { id: row.person_id }
+          },
+          body: {
+            data: {
+              email_1: email_1 || contact_info.email_1,
+              email_2: email_2 || contact_info.email_2
             }
           }
-        } else {
-          spinner.warn(
-            `${person.username} is missing ${
-              contact_info?.email_1
-                ? 'email_2'
-                : contact_info?.email_2
-                  ? 'email_1'
-                  : 'email_1 and email_2'
-            })
-              .join(' and ')}`
+        });
+        if (error) {
+          spinner.fail(
+            `${spinner.text}: ${Colors.error(`Error ${error.error_id}: ${error.error}`)}`
           );
+          continue;
         }
+        spinner.succeed(
+          `${spinner.text}: ${Colors.varName(
+            'email_1'
+          )} = ${email_1 || contact_info.email_1}, ${Colors.varName(
+            'email_2'
+          )} = ${email_2 || contact_info.email_2}`
+        );
+      } else {
+        spinner.info(
+          `${spinner.text}: ${Colors.varName(
+            'email_1'
+          )} = ${email_1 || contact_info.email_1}, ${Colors.varName(
+            'email_2'
+          )} = ${email_2 || contact_info.email_2} [${Colors.value('Dry run')}]`
+        );
       }
-      done = data.length < PAGE_SIZE;
-    } while (!done);
+    } else {
+      spinner.warn(`No contact info record found for ${spinner.text}`);
+      continue;
+    }
   }
 }

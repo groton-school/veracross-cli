6cfad164fe8e3c9b2855d1cbc1d19ae7285f7d08
Seth Battis <seth@battis.net>
2026-08-20 08:30:04 -0400

    feat: vc classes lms redirect now accepts a CSV of classes

--- packages/veracross-cli/src/Commands/Classes/LMS/Redirect.ts
+++ packages/veracross-cli/src/Commands/Classes/LMS/Redirect.ts
@@ -1,4 +1,10 @@
-import { URLString } from '@battis/descriptive-types';
+import fs from 'node:fs';
+import path from 'node:path';
+import {
+  DateTimeString,
+  PathString,
+  URLString
+} from '@battis/descriptive-types';
 import { Colors } from '@qui-cli/colors';
 import { Log } from '@qui-cli/log';
 import * as Plugin from '@qui-cli/plugin';
@@ -7,17 +13,39 @@ import puppeteer from 'puppeteer';
 
 import '@qui-cli/env';
 
-export type Configuration = Plugin.Configuration & {
-  schoolRoute?: string;
-  username?: string;
-  password?: string;
-  internalClassId?: number;
+import { Positionals } from '@qui-cli/core';
+import { parse, stringify } from 'csv/sync';
+import { CSV } from '../../../lib/index.js';
+
+type ClassRedirect = {
+  internal_class_id: number;
   page?: string;
-  url?: URLString;
+  url: URLString;
 };
 
+type ClassResult = ClassRedirect & {
+  created: DateTimeString;
+};
+
+export type Configuration = Plugin.Configuration & {
+  pathToCsv?: PathString;
+  school_route?: string;
+  username?: string;
+  password?: string;
+} & Partial<ClassRedirect>;
+
+Positionals.require({
+  pathToCsv: {
+    description: 'Path to a CSV file of redirect information'
+  }
+});
+Positionals.allowOnlyNamedArgs();
+Positionals.requireAtLeast(0);
+
+const DEFAULT_PAGE_NAME = 'Redirect';
+
 const config: Configuration = {
-  page: 'Redirect'
+  page: DEFAULT_PAGE_NAME
 };
 
 export function configure(proposal: Configuration = {}) {
@@ -35,7 +63,7 @@ export function options() {
       schoolRoute: {
         description: `Veracross school route short code`,
         hint: Colors.quotedValue(`"schoolname"`),
-        default: config.schoolRoute
+        default: config.school_route
       },
       username: {
         description: 'Veracross user name',
@@ -60,36 +88,49 @@ export function options() {
     num: {
       internalClassId: {
         description: `Veracross internal class ID to set redirect on`,
-        default: config.internalClassId
+        default: config.internal_class_id
       }
     }
   };
 }
 
-export function init({ values }: Plugin.ExpectedArguments<typeof options>) {
-  configure(values);
+export function init({
+  values: {
+    internalClassId: internal_class_id,
+    schoolRoute: school_route,
+    ...rest
+  }
+}: Plugin.ExpectedArguments<typeof options>) {
+  const pathToCsv = Positionals.get('pathToCsv');
+  configure({ pathToCsv, school_route, internal_class_id, ...rest });
 }
 
 export async function run() {
-  const { schoolRoute, internalClassId, url, page } = config;
-  if (!schoolRoute) {
+  const { school_route, internal_class_id, url, page } = config;
+  if (!school_route) {
     Log.fatal(`${Colors.optionArg('schoolRoute')} must be defined`);
     process.exit(1);
   }
-  if (!internalClassId) {
-    Log.fatal(`${Colors.optionArg('internalClassid')} must be defined`);
-    process.exit(2);
-  }
-  if (!url) {
-    Log.fatal(`${Colors.optionArg('url')} must be defined`);
-    process.exit(3);
-  }
-  if (!page) {
-    Log.fatal(`${Colors.optionArg('page')} must be defined`);
-    process.exit(4);
+
+  const data: ClassRedirect[] =
+    internal_class_id && url ? [{ internal_class_id, page, url }] : [];
+  let outputPath: string | undefined = undefined;
+  const result: ClassResult[] = [];
+
+  if (config.pathToCsv) {
+    const filePath = path.resolve(process.cwd(), config.pathToCsv);
+    outputPath = path.join(
+      path.dirname(filePath),
+      `${path.basename(filePath, path.extname(filePath))}.result.csv`
+    );
+    const csv = await parse<ClassRedirect>(fs.readFileSync(filePath, 'utf8'), {
+      columns: true,
+      cast: CSV.cast({ internal_class_id: 'int' })
+    });
+    data.push(...csv);
   }
 
-  const spinner = ora('Awaiting interactive login').start();
+  const spinner = ora('Awaiting authentication ').start();
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -98,9 +139,7 @@ export async function run() {
   const [tab] = await browser.pages();
 
   try {
-    const websiteAdminUrl = `https://portals-embed.veracross.com/${config.schoolRoute}/course/${config.internalClassId}/website-admin/pages`;
-    await tab.goto(websiteAdminUrl);
-
+    await tab.goto(`https://portals.veracross.com/${school_route}`);
     if (config.username) {
       spinner.text = 'Entering username';
       await tab.locator('#username').fill(config.username);
@@ -124,98 +163,104 @@ export async function run() {
           : 'Waiting for MFA';
     }
 
-    await tab.locator('#add-item').wait();
+    spinner.text = 'Waiting for portal authentication';
+    await tab.locator('.vx-system-nav').wait();
     spinner.succeed('Authenticated');
 
-    spinner.start(`Creating ${Colors.value(page)} redirect page`);
-    await tab.locator('#add-item').click();
-    await tab.locator('.modal-content input[type="text"]').fill(page);
-    await tab.locator('.modal-content a.button.add.publish').click();
+    for (const { internal_class_id, page = DEFAULT_PAGE_NAME, url } of data) {
+      spinner.start(`Creating ${Colors.value(page)} redirect page`);
+      const websiteAdminUrl = `https://portals-embed.veracross.com/${school_route}/course/${internal_class_id}/website-admin/pages`;
+      await tab.goto(websiteAdminUrl);
 
-    spinner.text = 'Adding embed code';
-    const composer = await tab.waitForFrame(async (frame) => {
-      const frameElement = await frame.frameElement();
-      if (!frameElement) {
-        return false;
+      await tab.locator('#add-item').click();
+      await tab.locator('.modal-content input[type="text"]').fill(page);
+      await tab.locator('.modal-content a.button.add.publish').click();
+
+      spinner.text = 'Adding embed code';
+      const composer = await tab.waitForFrame(async (frame) => {
+        const frameElement = await frame.frameElement();
+        if (!frameElement) {
+          return false;
+        }
+        const id = await frameElement.evaluate((el) => el.getAttribute('id'));
+        return id === 'composer-frame';
+      });
+      if (!composer) {
+        throw new Error();
       }
-      const id = await frameElement.evaluate((el) => el.getAttribute('id'));
-      return id === 'composer-frame';
-    });
-    if (!composer) {
-      throw new Error();
-    }
-    await composer.locator('.add-button[data-type="embed-code"]').click();
-    const embed = await composer.waitForSelector(
-      '.preview .section:has(.embed) .section-button.edit'
-    );
-    if (!embed) {
-      throw new Error('Embed code button not found');
-    }
-    await embed.evaluate((el) => el.click());
-
-    await composer
-      .locator('.sidebar .editor-field textarea.embed-control')
-      .fill(
-        `<iframe width="100%" height="300" scrolling="no" frameborder="no" src="${url}"></iframe>`
+      await composer.locator('.add-button[data-type="embed-code"]').click();
+      const embed = await composer.waitForSelector(
+        '.preview .section:has(.embed) .section-button.edit'
       );
-    await tab.waitForNavigation();
+      if (!embed) {
+        throw new Error('Embed code button not found');
+      }
+      await embed.evaluate((el) => el.click());
 
-    spinner.text = `Publishing ${Colors.value(page)} page`;
-    await tab.goto(websiteAdminUrl);
-    const draftsHandle = await tab.waitForSelector('.pages .page.draft');
-    if (!draftsHandle) {
-      throw new Error('Drafts not found');
-    }
-    const draft = (await tab.$$('.page.draft')).find((draft) =>
-      draft.evaluate(
-        (el, ...[page]) => el.querySelector('.title').innerText === page,
-        page
-      )
-    );
-    if (!draft) {
-      throw new Error(`Draft ${Colors.value(page)} page not found`);
-    }
-    const publish = await draft.$('.date .button');
-    if (!publish) {
-      throw new Error('Publish button not found');
-    }
-    await publish.click();
-    await tab.locator('.modal-content .button.add.publish').click();
+      await composer
+        .locator('.sidebar .editor-field textarea.embed-control')
+        .fill(
+          `<iframe width="100%" height="300" scrolling="no" frameborder="no" src="${url}"></iframe>`
+        );
+      await composer.waitForSelector('.last-saved:has(.fa.fa-pencil', {
+        hidden: true
+      });
 
-    spinner.text = `Making ${Colors.value(page)} page the class home page`;
-    await tab.waitForSelector('.pages .page.published');
-    const published = (await tab.$$('.pages .page.published')).find((pub) =>
-      pub.evaluate(
-        (el, ...[page]) => el.querySelector('.title').innerText === page,
-        page
-      )
-    );
-    if (!published) {
-      throw new Error(`Published ${Colors.value(page)} page not found`);
-    }
-    const options = await published.$('.button.options');
-    if (!options) {
-      throw new Error('Options button not found');
-    }
-    await options.click();
-    await published.waitForSelector('.button:has(.fa.fa-star)');
-    await tab
-      .locator('.pages .page.published .button:has(.fa.fa-star)')
-      .click();
+      spinner.text = `Publishing ${Colors.value(page)} page`;
+      await composer.locator('.publish.action-button.add').click();
+      await tab.locator('.modal-content .button.add.publish').click();
+      await tab.waitForNavigation();
 
-    spinner.text = 'Saving changes';
-    const done = await published.waitForSelector(
-      '.homepage-indicator:has(.fa.fa-star)'
-    );
-    if (!done) {
-      throw new Error('Homepage status not verified');
+      spinner.text = `Making ${Colors.value(page)} page the class home page`;
+      await tab.waitForSelector('.pages .page.published');
+      const published = (await tab.$$('.pages .page.published')).find((pub) =>
+        pub.evaluate(
+          (el, ...[page]) => el.querySelector('.title').innerText === page,
+          page
+        )
+      );
+      if (!published) {
+        throw new Error(`Published ${Colors.value(page)} page not found`);
+      }
+      const options = await published.$('.button.options');
+      if (!options) {
+        throw new Error('Options button not found');
+      }
+      await options.click();
+      await published.waitForSelector('.button:has(.fa.fa-star)');
+      await tab
+        .locator('.pages .page.published .button:has(.fa.fa-star)')
+        .click();
+
+      spinner.text = 'Saving changes';
+      const done = await published.waitForSelector(
+        '.homepage-indicator:has(.fa.fa-star)'
+      );
+      if (!done) {
+        throw new Error('Homepage status not verified');
+      }
+
+      spinner.text = 'Saving result';
+      result.push({
+        internal_class_id,
+        page,
+        url,
+        created: new Date().toISOString()
+      });
+      if (outputPath) {
+        fs.writeFileSync(outputPath, stringify(result, { header: true }));
+      }
+      spinner.succeed(`${Colors.value(page)} page redirected`);
     }
     await tab.close();
-    spinner.succeed(`${Colors.value(page)} page redirected`);
   } catch (error) {
     spinner.fail();
     Log.error(Colors.error((error as Error).message));
   } finally {
     await browser.close();
+  }
+
+  if (outputPath) {
+    Log.info(`Creation timestamps written to ${Colors.path(outputPath)}`);
   }
 }
